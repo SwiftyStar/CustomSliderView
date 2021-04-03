3072fcde8507d6936caf292b9bd61ca383d54278
Jacob Starry <jstarry@mail.bradley.edu>
2021-04-02 19:40:22 -0500

    Initial Commit

---
+++ CustomSlider/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  CustomSlider
+//
+//  Created by Jacob Starry on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
